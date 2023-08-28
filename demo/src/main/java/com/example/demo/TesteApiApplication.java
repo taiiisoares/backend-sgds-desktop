1c6e80ab32c9a31a187eca0b28e6a4ed013b7a99
PHRGusmao <phrgusmao@gmail.com>
2023-08-28 15:48:37 -0300

    Adesão da tela de login

--- demo/src/main/java/com/example/demo/TesteApiApplication.java
+++ demo/src/main/java/com/example/demo/TesteApiApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import java.util.List;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.neo4j.ConfigBuilderCustomizer;
@@ -14,11 +17,30 @@ import com.example.demo.entidade.Usuario;
 public class TesteApiApplication {
 
 	public static void main(String[] args) {
-		Usuario usuario = new Usuario();
-		ConfigurableApplicationContext context = SpringApplication.run(TesteApiApplication.class, args);
-		ApiService apiService = context.getBean(ApiService.class);
-		List<Usuario> user = apiService.getUsuarioByTelefone("551124510987");
-		usuario.printUsuarioList(user);
+	
+		Thread springThread = new Thread(() -> {
+			Usuario usuario = new Usuario();
+			ConfigurableApplicationContext context = SpringApplication.run(TesteApiApplication.class, args);
+			ApiService apiService = context.getBean(ApiService.class);
+			List<Usuario> user = apiService.getUsuarioByTelefone("551124510987");
+			usuario.printUsuarioList(user);
+		});
+
+        Thread swingThread = new Thread(() -> {
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+			}
+				java.awt.EventQueue.invokeLater(() -> {
+				Login login = new Login();
+				login.setLocationRelativeTo(null);
+				login.setVisible(true);
+			});
+        });
+
+		swingThread.start();
+		
+		springThread.start();
 	}
 
 }
